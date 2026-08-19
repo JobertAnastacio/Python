c7c316b2671af2912e4c1392d22d516b284ab501
Jôbert Anastácio <jobert14anastacio@gmail.com>
2026-08-19 18:53:10 -0300

    Update ex077.py

--- estudos/ex077.py
+++ estudos/ex077.py
@@ -5,10 +5,17 @@ for c in range(0,len(palavras)):
         if palavras[c][v] == 'a':
           print(palavras[c][v],end=' ')
         if palavras[c][v] == 'e':
-          print(palavras[c][v],end=' ')
+            print(palavras[c][v],end=' ')
         if palavras[c][v] == 'i':
-          print(palavras[c][v],end=' ')
+            print(palavras[c][v],end=' ')
         if palavras[c][v] == 'o':
-          print(palavras[c][v],end=' ')
+            print(palavras[c][v],end=' ')
         if palavras[c][v] == 'u':
-          print(palavras[c][v],end=' ')
+            print(palavras[c][v],end=' ')
+
+#outra forma mais simples de fazer o mesmo que o código acima
+'''for p in palavras:
+    print(f'\n{p.upper()} temos: ',end=' ')
+    for vogal in p:
+        if vogal.lower() in 'aeiou':
+            print(vogal,end=' ')'''
